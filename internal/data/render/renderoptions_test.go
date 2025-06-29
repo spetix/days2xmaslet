cc144d01993dde419bdac07b64a2ef5cb6bc2a6e
spetix <christian@cassano.fe.it>
2025-06-30 00:13:56 +0200

    missing files

---
+++ internal/data/render/renderoptions_test.go
@@ -0,0 +1,44 @@
+package render
+
+import (
+	"testing"
+	"time"
+
+	"github.com/spetix/days2xmasleft/internal/dateutil"
+)
+
+type dateAndExpectedDays struct {
+	Date                    time.Time
+	ExpectedDays            time.Duration
+	Unit                    time.Duration
+	ExpectedFormattedResult string
+}
+
+var currentDateAndExpected = []dateAndExpectedDays{
+	{time.Date(2024, 12, 24, 0, 0, 0, 0, time.UTC), dateutil.Day, dateutil.Day, "1"},
+	{time.Date(2024, 12, 25, 0, 0, 0, 0, time.UTC), 0, dateutil.Day, "0"},
+	{time.Date(2024, 12, 26, 0, 0, 0, 0, time.UTC), 364 * dateutil.Day, dateutil.Day, "364"},
+	{time.Date(2024, 12, 27, 0, 0, 0, 0, time.UTC), 363 * dateutil.Day, dateutil.Day, "363"},
+	{time.Date(2019, 12, 28, 0, 0, 0, 0, time.UTC), 363 * dateutil.Day, dateutil.Day, "363"},
+}
+
+func TestGetUnit(t *testing.T) {
+	r := RenderOptions{Unit: dateutil.Day}
+	unit := r.FormatData(
+
+	)
+	if unit != dateutil.Day {
+		t.Fail()
+	}
+	unit = GetUnit("h")
+	if unit != time.Hour {
+		t.Fail()
+	}
+	unit = GetUnit("m")
+		t.Fail()
+	}
+	unit = GetUnit("s")
+	if unit != time.Second {
+		t.Fail()
+	}
+}
